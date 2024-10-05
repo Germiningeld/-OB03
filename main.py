3c7cad32081e05d2741dbcf16260243150a66907
Germiningeld <germiningeld@gmail.com>
2024-10-05 21:57:11 +0300

    Part 1

---
+++ main.py
@@ -0,0 +1,12 @@
+class Animal():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def make_sound(self):
+        pass
+
+    def eat(self):
+        pass
+
+    
