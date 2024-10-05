1fa3b8b48453b39f2b625c52ef037e9f3c2032f5
Germiningeld <germiningeld@gmail.com>
2024-10-05 22:10:29 +0300

    Part 2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Animal():
-    def __init__(self, name, age):
+    def __init__(self, name:str, age:int):
         self.name = name
         self.age = age
 
@@ -9,4 +9,36 @@ class Animal():
     def eat(self):
         pass
 
-    
+class Bird(Animal):
+    def __init__(self, name, age, color:str):
+        super().__init__(name, age)
+        self.color = color
+
+    def make_sound(self):
+        print("Чирик")
+
+    def eat(self):
+        print("Я ем зерна")
+
+class Mammal(Animal):
+    def __init__(self, name, age, size:str):
+        super.__init__(name, age)
+        self.size = size
+
+    def make_sound(self):
+        print("Гррр")
+
+    def eat(self):
+        print("Я ем траву")
+
+class Reptile(Animal):
+    def __init__(self, name, age, legs:bool):
+        super().__init__(name, age)
+        self.legs = legs
+
+    def make_sound(self):
+        print("Шшшшш")
+
+    def eat(self):
+        print("Я ем насекомых")
+
