a8084cb24e4951a6cc62fc72a8e66aa647ee5223
Germiningeld <germiningeld@gmail.com>
2024-10-06 10:37:14 +0300

    Part 4

--- main.py
+++ main.py
@@ -42,6 +42,55 @@ class Reptile(Animal):
     def eat(self):
         print("Я ем насекомых")
 
+def animal_sound(animals):
+    for animal in animals:
+        animal.make_sound()
+
+
+class Worker():
+    def __init__(self, name:str, experience:int):
+        self.name = name
+        self.age = experience
+
+class ZooKeeper(Worker):
+    def __init__(self, name, experience):
+        super().__init__(name, experience)
+
+    def feed_animal(self):
+        print("Я покормил животных")
+
+class Veterinarian(Worker):
+    def __init__(self, name, experience):
+        super().__init__(name, experience)
+
+    def feed_animal(self):
+        print("Я вылечил животного")
+
+
+class Zoo():
+    def __init__(self, name:str, adress:str):
+        self.name = name
+        self.adress = adress
+        self.animals = []
+        self.workers = []
+
+
+    def add_animal(self, name, age):
+        self.animals.append(Animal(name, age))
+
+    def add_worker(self, name, experience):
+        self.workers.append(Worker(name, experience))
+
+    def print_info(self):
+        if len(self.animals) > 0:
+            print("Животные:")
+            for animal in self.animals:
+                print(f'{animal.name}, {animal.age}')
+
+        if len(self.workers) > 0:
+            print("Сотрудники:")
+            for worker in self.workers:
+                print(f'{worker.name}, {worker.age}')
 
 
 bird = Bird("Воробей", 1, "Серый")
@@ -50,9 +99,6 @@ reptile = Reptile("Кобра", 2, False)
 
 
 
-def animal_sound(animals):
-    for animal in animals:
-        animal.make_sound()
 
 animals_list = [
     bird,
@@ -60,4 +106,10 @@ animals_list = [
     reptile
 ]
 
+zoo = Zoo('ZeroZoo', 'пр. Науки, 1')
+zoo.add_animal('Слон', 48)
+zoo.add_animal('Жираф', 27)
+zoo.add_worker('Витек', 69)
+
 animal_sound(animals_list)
+zoo.print_info()
